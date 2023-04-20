f16578c30b403116b12026dcfb03e9644c28a775
uhidontkno <86028086+uhidontkno@users.noreply.github.com>
2023-04-20 01:12:35 +0000

    Initial commit

---
+++ front/script.js
@@ -0,0 +1,63 @@
+function disp() {
+  const dropdown = document.getElementById('proxy');
+const type = dropdown.value;
+  if (type != "") {
+  var btn = document.querySelector(".btn");
+  var audio = new Audio('assets/click.mp3');
+  
+  audio.play();
+  btn.disabled = true;
+  btn.innerText = "..."
+  setTimeout(() => {
+    var txt = document.querySelector(".dtxt");
+    txt.className = "dtxt-d";
+  },100)
+  }
+    var resCode = 0;
+var resData= "";
+  var now = Date.now();
+   if (type == "") {
+      popup(`<img src="assets/Chest.gif" style="width:100%;height:128px; object-fit: contain;"></img><h3 style="text-align:center;">Error</h3><p>An error has occurred. More details:<br></p><pre>You haven't picked anything in the proxy dropdown!</pre>`);
+     
+    } else {
+fetch('/api/fetchp?type=' + type + '&time=' + now)
+    .then(response => {
+    console.log('Response status code:', response.status);
+        resCode=response.status;
+    return response.text();
+  })
+  .then(data => {
+    console.log('Response data:', data);
+      resData = data;
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+   
+  setTimeout(() => {
+
+
+  
+  if (resCode == 200) {
+    popup(`<img src="assets/Chest.gif" style="width:100%;height:128px; object-fit: contain;"></img><h3 style="text-align:center;">Your proxy:</h3><PRE>${resData}</PRE><br><p>You can claim more in 24 hours.</p>`);
+  }
+    if (resCode != 200) {
+      popup(`<img src="assets/Chest.gif" style="width:100%;height:128px; object-fit: contain;"></img><h3 style="text-align:center;">Error</h3><p>An error has occurred. More details:<br></p><pre>${resData}</pre>`);
+    }
+    var txt = document.querySelector(".dtxt-d");
+    var btn = document.querySelector(".btn");
+      btn.disabled = false;
+    txt.className = "dtxt";
+    btn.innerText = "Dispense!"
+  },1000)
+   }
+}
+function popup(txt) {
+  btnclose();
+  var b = document.querySelector('body');
+  b.innerHTML += `<div class="popup">${txt}<br><br> <button onclick="btnclose()">Close</button></div>`;
+}
+function btnclose() {
+  var b = document.querySelector('.popup');
+  if (b != null) {b.remove();}
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const rateLimit = require("express-rate-limit");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve the frontend from the "front" folder
+app.use('/', express.static(path.join(__dirname, 'front')));
+
+const apiLimiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, // 24 hours
+  max: 1, // limit each IP to 1 requests per windowMs
+  message: "You have ran out of uses for today. Please wait..."
+});
+
+app.get('/api/fetchp', apiLimiter, (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync('./prx/data.json', 'utf8'));
+    const type = req.query.type;
+    const now = Math.floor(Date.now() / 1000);
+    const time = parseInt(req.query.time);
+
+    if (isNaN(time) ||  now - time > 30) {
+      res.status(400).send("Request expired. TIP: Requests only last for 30 seconds.");
+      return;
+    }
+
+    if (!data[type]) {
+      res.status(400).send("Invalid type");
+      return;
+    }
+
+    const entries = data[type];
+    const randomIndex = Math.floor(Math.random() * entries.length);
+    const randomEntry = entries[randomIndex];
+
+    res.send(randomEntry);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
