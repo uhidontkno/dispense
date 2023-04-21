da67836d46879d2ffbebae1f68db95f68bccde40
uhidontkno <86028086+uhidontkno@users.noreply.github.com>
2023-04-21 15:18:42 +0000

    Make audio play a bit faster by preloading some assets.

--- front/script.js
+++ front/script.js
@@ -71,3 +71,107 @@ function btnclose() {
   var b = document.querySelector('.popup');
   if (b != null) {b.remove();}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+r = Math.floor(Math.random * 69);
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; //months from 1-12
+var day = dateObj.getUTCDate();
+var feulz = 0;
+if (month == 4 && day == 1) {
+  feulz = 1;
+}
+// a key map of allowed keys
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function activateCheats() {
+
+  alert("[ERR]: A critical error has occurred. Dispenser cannot operate at this moment.")
+  document.write("<video style='width:100%;height:100%;' autoplay controls  src='assets/a.mp4'></video>") // heheheha
+
+}
+if (r == 69 || feulz == 1) {
+  alert("[ERR]: A critical error has occurred. Dispenser cannot operate at this moment.")
+   document.write("<video style='width:100%;height:100%;' autoplay controls src='assets/a.mp4'></video>") // heheheha
+}
